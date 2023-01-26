7162a29259c2796ec12204fc2f77412a692cfaec
Erich Mostaco Buttner <erich.buttner@totvs.com.br>
2023-01-26 08:56:07 -0300

    Curso de fundamentos em reactjs

---
+++ src/components/Comment.jsx
@@ -0,0 +1,48 @@
+import { ThumbsUp, Trash } from 'phosphor-react'
+import styles from './Comment.module.css'
+import { Avatar } from './Avatar';
+import { useState } from 'react';
+
+export function Comment( { content, onDeleteComment } ) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        onDeleteComment(content)
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1
+        });
+    }
+
+    return (
+        <div className={styles.comment}>
+            <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/33982875?v=4" />
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header>
+                        <div className={styles.authorAndTime}>
+                            <strong>Erich Buttner</strong>
+                            <time title="20 de Janeiro as 07:30h" dateTime='2022-01-20 07:30:00'>Cerca de 1h atrás</time>
+                        </div>
+                    
+                        <button onClick={handleDeleteComment} title='Deletar comentário'>
+                            <Trash size={24} />
+                        </button>
+                    </header>
+
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+        </div>
+    )
+}
